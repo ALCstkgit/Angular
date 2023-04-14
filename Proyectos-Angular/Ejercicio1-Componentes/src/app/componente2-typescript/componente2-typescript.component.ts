f4c7f0f0168aa1d0af7dc3b1ace18e4ad9e60601
ALCstkgit <adrian.lopezc@softtek.com>
2023-04-14 11:03:01 +0200

    dia3CorreccionEj1

--- Proyectos-Angular/Ejercicio1-Componentes/src/app/componente2-typescript/componente2-typescript.component.ts
+++ Proyectos-Angular/Ejercicio1-Componentes/src/app/componente2-typescript/componente2-typescript.component.ts
@@ -7,12 +7,25 @@ import { Component } from '@angular/core';
 })
 export class Componente2TypescriptComponent {
   num1 : number = 5
-  cad : string = "hola"
+  cad1 : string = "hola"
+  cad2 : string = 'hola2'
+  //Los acentos invertidos te permiten introducir directamente el valor de las variables
+  cad3 : string = `${this.cad1}, ${this.cad2}, hola3`
+  b : boolean = true
+  nu : any = null
+  un : undefined
   ngOnInit(){
     console.log(this.mostrarInfo(this.num1));
+    console.log(this.num1)
+    console.log(this.cad1)
+    console.log(this.cad2)
+    console.log(this.cad3)
+    console.log(this.b)
+    console.log(this.nu)
+    console.log(this.un)
   }
 
   mostrarInfo(num:number){
-    return num + this.cad;
+    return num + this.cad1;
   }
 }
